1bd56187180b3b869df6e3152a35340c8139f152
nujiznaw8240 <nujiznaw8240@gmail.com>
2025-02-18 19:05:29 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+var button = document.getElementById("button");
+var userInputEl = document.getElementById("user-input");
+var onButtonClick = function(){
+    console.log("updated! ");
+    var inputText = document.getElementById("food").value;
+    userInputEl.textContent = inputText;
+};
+button.addEventListener("click", onButtonClick);
+
+// turn sound on and off
+var soundButtonPressed = 0;
+var soundButton = document.getElementById("sound-button");
+
+var audioCakeEl = document.getElementById("candle-sound");
+var audioFireworkEl = document.getElementById("firework-sound");
+
+var onSoundButtonClick = function() {
+    soundButtonPressed += 1;
+    if (soundButtonPressed%2) {
+        soundButton.textContent = "sound: on";
+        audioCakeEl.muted = false;
+        audioFireworkEl.muted = false;
+    }else{
+        soundButton.textContent = "sound: off";
+        audioCakeEl.muted = true;
+        audioFireworkEl.muted = true;
+    }
+}
+soundButton.addEventListener("click", onSoundButtonClick); 
+
+var playCakeAudio = function() {
+    $('#candle-sound').stop(true, true); // Stop any ongoing animation immediately
+    
+    audioCakeEl.volume = 0;
+    audioCakeEl.play();
+    $('#candle-sound').animate({volume: 1}, 1000);
+}
+var pauseCakeAudio = function() {
+    $('#candle-sound').stop(true, true);
+
+    audioCakeEl.volume = 1;
+    $('#candle-sound').animate({volume: 0}, 400, function() {
+        audioCakeEl.pause();
+    });
+}
+var playFireworkAudio = function() {
+    $('#firework-sound').stop(true, true); // Stop any ongoing animation immediately
+    
+    audioFireworkEl.volume = 0;
+    audioFireworkEl.play();
+    $('#firework-sound').animate({volume: 1}, 1000);
+}
+var pauseFireworkAudio = function() {
+    $('#firework-sound').stop(true, true);
+
+    audioFireworkEl.volume = 1;
+    $('#firework-sound').animate({volume: 0}, 400, function() {
+        audioFireworkEl.pause();
+    });
+}
